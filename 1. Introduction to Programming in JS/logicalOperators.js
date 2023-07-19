44d8ce537eadc3cdaa3e65a4148d9a974bb8639a
Rajat Sharma <rajatxd007@gmail.com>
2023-07-20 01:28:12 +0530

    Added Codes

---
+++ 1. Introduction to Programming in JS/logicalOperators.js
@@ -0,0 +1,17 @@
+//Logical Operators
+
+console.log(true && false);
+console.log(false || false);
+console.log(true && true);
+console.log((5<10) && (6<3));
+console.log(!(6>3));
+console.log(!(3>6));
+console.log(!true);
+console.log(!false);
+
+
+//ShortCircuiting Examples
+console.log(10 || 6);
+console.log(10 && 6);
+console.log(0 || -0);
+console.log(0 && 10);
---
+++ 1. Introduction to Programming in JS/operatorsDemo.js
@@ -0,0 +1,21 @@
+let x = 10;
+let y = 3;
+
+console.log(x+y); // addition
+console.log(x-y); //substraction
+console.log(x*y); //multiplication
+console.log(x/y); //returns the quotient
+console.log(x%y); //returns the remainder
+console.log(x**2); // exponent operator
+
+//Assignment Operators
+x += 2; // take previous value of a and add 2 to it and assign new value back to a
+console.log("value of a after addition is:",x);
+x %=5;
+console.log("Value after Modulo operator is:",x);
+x *= 2;
+console.log("Value after multiplication is:",x);
+x/=2;
+console.log("value after division is:",x);
+x**=2;
+console.log("Value after exponent is:",x);
