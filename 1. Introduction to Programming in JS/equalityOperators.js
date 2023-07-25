9e098830c6bc4a060258f6d67d01a948ba79bc2d
Rajat Sharma <rajatxd007@gmail.com>
2023-07-26 00:38:09 +0530

    Added Codes

---
+++ 1. Introduction to Programming in JS/equalityOperators.js
@@ -0,0 +1,10 @@
+console.log(1=='1');
+console.log(1=='Rajat');
+console.log(NaN == NaN);
+
+
+console.log(typeof 1);
+console.log(typeof "1");
+console.log(typeof null);
+console.log(typeof undefined);
+console.log(typeof false);
