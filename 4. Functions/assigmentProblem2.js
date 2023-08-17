45488e5ff63b666cae2ddaaacacc1e27756938f4
Rajat Sharma <rajatxd007@gmail.com>
2023-08-17 22:13:02 +0530

    Added Codes

---
+++ 4. Functions/assigmentProblem2.js
@@ -0,0 +1,43 @@
+//https://leetcode.com/problems/add-digits
+
+/*
+Given an integer num, repeatedly add all its digits until the result has only one digit, and return it.
+
+ 
+
+Example 1:
+
+Input: num = 38
+Output: 2
+Explanation: The process is
+38 --> 3 + 8 --> 11
+11 --> 1 + 1 --> 2 
+Since 2 has only one digit, return it.
+Example 2:
+
+Input: num = 0
+Output: 0
+ 
+
+Constraints:
+
+0 <= num <= 231 - 1
+
+*/
+
+function addSum(num) {
+    let s =0;
+    while(num>0){
+        let rem= num %10;
+        s = s + rem;
+        num = parseInt(num/10);
+    }
+    //console.log(s);
+    if(s>9){
+        return addSum(s);
+    }else{
+        return s;
+    }
+}
+
+console.log(addSum(228));
